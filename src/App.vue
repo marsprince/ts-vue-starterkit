323dac41d668eb54988f2863507c9a0b21c9b1e1
liujia02 <liujia02@renrenche.com>
2018-10-17 18:47:35 +0800

    add stylelint

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
@@ -17,11 +18,14 @@
   color: #2c3e50;
   font-size: 28px;
 }
+
 #nav {
   padding: 30px;
+
   a {
     font-weight: bold;
     color: #2c3e50;
+
     &.router-link-exact-active {
       color: #42b983;
     }
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, { RouterOptions } from 'vue-router';
 import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const routerOptions: RouterOptions = {
   mode: 'history',
   // base: '',
   routes: [
@@ -19,7 +19,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
   ],
-});
+};
+
+const router: Router = new Router(routerOptions);
+
+export default router;
